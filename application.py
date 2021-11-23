366f4330649a12f070d1d52dcfbaf565928878f0
Tyler-Coombs <84094311+Tyler-Coombs@users.noreply.github.com>
2021-11-22 20:59:01 -0330

    Further developed application file, templates, and created database.
    Created database to store album information and file name. 
    Developed templates for each page of the website.
    Worked on code to get information from DB and images from folder to display on Albums page.

--- application.py
+++ application.py
@@ -1,26 +1,44 @@
+import sqlite3
+from contextlib import closing
+import imghdr
 import os
 from flask import Flask, render_template, redirect, request, abort
-import sqlite3
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_PATH'] = 'static/album_covers'
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jfif']
+
+conn = sqlite3.connect("albums.db", check_same_thread=False)
 
 
+@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/albums")
 def albums():
-    pass
+    with closing(conn.cursor()) as c:
+        query = "SELECT * from Albums"
+        c.execute(query)
+        results = c.fetchall()
+        images = []
+        for result in results:
+            images.append((result[1], result[2], result[3], result[4]))
+    return render_template("albums.html", images=images)
+
 
 @app.route("/update")
 def update():
-    pass
+    return render_template("update.html")
+
 
 @app.route("/remove")
 def remove():
-    pass
-
+    return render_template("remove.html")
 
 
 def validate_album(stream):
